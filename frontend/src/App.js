dd168147df7e2844237a2388103963130a3cfe04
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2025-02-19 23:45:02 +0000

    basic home page renders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/Homepage';
+import Navbar from './components/Navbar';
+import ContactUs from "./components/ContactUs";
+import {BrowserRouter as Router} from "react-router-dom";
+import { Route, Routes} from "react-router";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Navbar/> */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path='/contactus' element={<ContactUs />}></Route> */}
+      </Routes>
+    </Router>
   );
 }
 
