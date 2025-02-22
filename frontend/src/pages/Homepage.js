794e2bbb6bd311279e616e14edad436b7049fc43
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2025-02-22 19:07:25 +0000

    changed tailwind config file

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Homepage = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1 className="text-2xl">Home Page</h1>
         </div>
     );
 };
