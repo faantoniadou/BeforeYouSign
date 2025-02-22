374980bbf9ae076bf646360e2aa9be5c8e50f3f4
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2025-02-22 17:39:04 +0000

    ran npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9

---
+++ frontend/webpack.config.js
@@ -0,0 +1,8 @@
+// export const module = {
+//   rules: [
+//     {
+//       test: /\.css$/,
+//       use: ['style-loader', 'css-loader'], // Ensures CSS is still processed
+//     },
+//   ],
+// };
