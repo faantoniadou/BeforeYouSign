69b87cfcb36dedc9a043a03d7f67d5991ced0cd6
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2025-02-23 00:13:31 +0000

    small syntax change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Navbar = () => (
-  <header className="bg-white sticky top-0 w-full shadow-md">
-    <nav className="flex justify-center py-4">
-      <a href="#home" className="mx-4 hover-scale-25 text-gray-800 hover:text-gray-600">Home</a>
-      <a href="#location-finder" className="mx-4 text-gray-800 hover:text-gray-600">Location Finder</a>
-      <a href="#blog" className="mx-4 text-gray-800 hover:text-gray-600">Blog</a>
-      <a href="#contact" className="mx-4 text-gray-800 hover:text-gray-600">Contact</a>
-    </nav>
-  </header>
-);
+function Navbar() {
+  return <header className="bg-white sticky top-0 w-full shadow-md">
+       <nav className="flex justify-center py-4">
+         {/* <a href="#home" className="mx-4 hover-scale-25 text-gray-800 hover:text-gray-600">Home</a> */}
+         <a href="#location-finder" className="mx-4 text-gray-800 hover:text-gray-600">Location Finder</a>
+         <a href="#blog" className="mx-4 text-gray-800 hover:text-gray-600">Blog</a>
+         <a href="#contact" className="mx-4 text-gray-800 hover:text-gray-600">Contact</a>
+       </nav>
+     </header>
+}
 
 export default Navbar;
