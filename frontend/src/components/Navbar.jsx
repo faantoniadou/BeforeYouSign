5a85745a9713c3beba3df625bd944d73ed442ec5
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2025-02-23 01:24:36 +0000

    small corrections and new files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 function Navbar() {
   return <header className="bg-white sticky top-0 w-full shadow-md">
        <nav className="flex justify-center py-4 space-x-5">
          {/* <a href="#home" className="mx-4 hover-scale-25 text-gray-800 hover:text-gray-600">Home</a> */}
          <div>
           <div className="space-x-4">
-            <button href="#location-finder" type="button" className="rounded-full hover:bg-gray-100 p-1">
+            <button href="#location-finder" type="button" className="rounded-full hover:bg-gray-100 p-2">
               <span className="p-1">
                 Location Finder
               </span>
@@ -16,7 +14,7 @@ function Navbar() {
 
          <div>
           <div className="">
-            <button href="#blog" type="button" className="rounded-full hover:bg-gray-100 p-1">
+            <button href="#blog" type="button" className="rounded-full hover:bg-gray-100 p-2">
               <span className="p-1">
                 Blog
               </span>
@@ -26,7 +24,7 @@ function Navbar() {
 
          <div>
           <div className="">
-            <button href="#contact" type="button" className="rounded-full hover:bg-gray-100 p-1">
+            <button href="#contact" type="button" className="rounded-full hover:bg-gray-100 p-2">
               <span className="p-1">
                 Contact Us
               </span>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 export const purge = ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'];
 export const darkMode = false;
 export const theme = {
-  extend: {},
 };
 export const variants = {
   extend: {},
