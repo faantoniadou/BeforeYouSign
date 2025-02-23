4e38015c7b529c70b3e4163af846cea6ed3c6bf0
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2025-02-23 00:05:31 +0000

    added basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import './index.css';
 function App() {
   return (
     <Router>
-      {/* <Navbar/> */}
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Homepage = () => {
     return (
         <div>
-            <h1 className="bg-pink-500 text-black p-4">Home Page does nt</h1>
+            <h1 className="bg-pink-200 text-black p-9">Home Page; this just checks that tailwind works</h1>
         </div>
     );
 };
